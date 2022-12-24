e0c1b8b7d2a2d856c1d2bd344c7821bc26318231
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2022-12-24 17:06:05 +0200

    Add modules

---
+++ module_1/1.2.rb
@@ -0,0 +1,15 @@
+"""
+У змінну first_number записати число 10, у змінну second_number записати число 3. 
+Створити третю змінну result, в яку записати результат множення змінних first_number та second_number. 
+Вивести в консоль значення змінної result.
+Написати замість #type your code here реалізацію поставленої задачі. 
+"""
+
+def mult()
+    first_number = 10
+    second_number = 3
+    
+    result = first_number * second_number
+    
+    puts(result)
+end
