5444d5fec78dc2e46570af776f06367a44457f2f
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-01-21 23:02:47 +0200

    add filter for operations in reports

--- financehub/app/controllers/reports_controller.rb
+++ financehub/app/controllers/reports_controller.rb
@@ -1,13 +1,21 @@
 class ReportsController < ApplicationController
+
   def index
   end
 
   def report_by_category
-    @operations = Operation.
+    # puts '______________________'
+    # puts params
+    # puts '______________________'
+    @operations = Operation.filter_by_category_id(params[:category_id]).filter_by_start_date(params[:start_date]).filter_by_final_date(params[:final_date])
+    # puts "============================="
+    # p @operations
+    # puts "============================="
     render "report_by_category"
   end
 
   def report_by_dates
     render "report_by_dates"
   end
+
 end
---
+++ financehub/public/assets/application-7ab48221ba8c81e6f73c5abb6518714bc862ed65fcfc85415e6fe77fa8fc7c6d.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery"
+import "popper"
+import "bootstrap-sprockets"
+import "@rails/actiontext"
+import "trix"
+import "@hotwired/turbo-rails"
+import "controllers";
