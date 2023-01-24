f1e941c6fa7a0c30e0a6983188b7daa7e8185de7
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-01-24 21:26:35 +0200

    fix bug with bootstrap and change header and logo color

--- financehub/public/assets/application-7ab48221ba8c81e6f73c5abb6518714bc862ed65fcfc85415e6fe77fa8fc7c6d.js
+++
@@ -1,8 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "jquery"
-import "popper"
-
-import "@rails/actiontext"
-import "trix"
-import "@hotwired/turbo-rails"
-import "controllers";
