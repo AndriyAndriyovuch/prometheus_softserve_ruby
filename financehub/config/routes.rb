2fe5705430205c07fe2694a97ad0131bfb1a7b99
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-02-03 17:04:36 +0200

    add prod setup and custom 404 page

--- financehub/config/routes.rb
+++ financehub/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :operations
   resources :categories
 
+  get '/404', to: 'errors#not_found'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
