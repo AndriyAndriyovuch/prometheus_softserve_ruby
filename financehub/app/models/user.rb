5c18667aeb85a048b0b3973f5e3633b9371d0236
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-02-05 21:33:44 +0200

    fix bug that user can't delete account

--- financehub/app/models/user.rb
+++ financehub/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :categories
-  has_many :operations
+  has_many :categories, dependent: :destroy
+  has_many :operations, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
