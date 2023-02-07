b0a4e82acd8570f30d51d593bb16bf9be76f509f
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-02-07 17:17:34 +0200

    add logic gor main page

--- financehub/app/controllers/main_controller.rb
+++ financehub/app/controllers/main_controller.rb
@@ -2,5 +2,10 @@ require 'open-uri'
 
 class MainController < ApplicationController
   def index
+    @operations = Operation.where('user_id = ?', current_user.id).order(:odate).last(10).reverse
+    puts('-' * 50)
+    puts @operations
+    puts('-' * 50)
+
   end
 end
