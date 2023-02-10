bd0e240eefb750e76da46569ebcab99b007ae928
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-02-10 14:34:24 +0200

    fix bug with main page (last operations)

--- financehub/app/controllers/main_controller.rb
+++ financehub/app/controllers/main_controller.rb
@@ -2,7 +2,9 @@ require 'open-uri'
 
 class MainController < ApplicationController
   def index
-    @operations = Operation.where('user_id = ? AND income = ?', current_user.id, false)
-                  .order(:odate).last(10).reverse
+    if user_signed_in?
+      @operations = Operation.where('user_id = ? AND income = ?', current_user.id, false)
+                    .order(:odate).last(10).reverse
+    end
   end
 end
