072f8a65700b54fe641b0fa827ca48807776ef82
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-02-10 17:27:55 +0200

    add some view logic

--- financehub/app/controllers/categories_controller.rb
+++ financehub/app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all.where('user_id = ?', current_user.id)
+    @categories = Category.all.where('user_id = ?', current_user.id).order(:id).page params[:page]
   end
 
   # GET /categories/1 or /categories/1.json
--- financehub/app/models/category.rb
+++ financehub/app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    # scope :filter_category_by_user, -> (user_id) { where "user_id = ?", user_id }
+    paginates_per 9
 
     belongs_to :user
     has_many :operations, dependent: :destroy
