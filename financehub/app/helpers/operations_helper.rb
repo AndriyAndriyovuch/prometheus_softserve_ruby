436a8e49135b086bf2fbf83f81acaece6380d69b
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-02-10 17:58:38 +0200

    add operation helper

--- financehub/app/helpers/operations_helper.rb
+++ financehub/app/helpers/operations_helper.rb
@@ -1,2 +1,16 @@
 module OperationsHelper
+    def format_description(operation)
+        if current_page?(operations_path) 
+            truncate operation.description, length: 20
+        else
+            operation.description
+        end
+    end
+
+
+    def show_operation_type(operation)
+        if params[:o_type] == nil
+            operation.income? ? "Income" : "Oulay"
+        end
+    end
 end
